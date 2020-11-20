a725420ed158dddbadb34a162b0c1b085e958cc5
John Mager <jmageriii@gmail.com>
2020-11-20 10:23:41 -0500

    disable grafana

--- logical-machines/tattletale.nix
+++ logical-machines/tattletale.nix
@@ -163,12 +163,6 @@
     '';
   };
 
-  # nix.nixPath = [
-  #   "local=/nixpkgs"
-  #   # "nixos-config=/etc/nixos/configuration.nix"
-  #   # "/nix/var/nix/profiles/per-user/root/channels"
-  # ];
-
   services = {
     grafana.enable = true;
 
