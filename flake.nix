483bbca21fe52fe32d71948025e63a2481bc4e0e
John Mager <jmageriii@gmail.com>
2021-10-30 13:01:43 -0400

    chore: update lock

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   inputs.secrets.url = "/etc/nixos/private/";
 
